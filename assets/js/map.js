c0eece4b897282d46bbb2d48bf8df565019611f1
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-12-09 11:25:13 -0600

    added darkmode feature to index.html, map.js and style.css

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,3 +1,18 @@
+// selectors
+var checkbox = document.querySelector('#toggle')
+var html = document.querySelector('html')
+
+// toggle between light and dark mode for the html page function
+var toggleDarkMode = function(){
+    // if input is checked then set dark mode if not set light mode
+  checkbox.checked ? html.setAttribute('class', 'dark'): html.removeAttribute('class','dark')
+}
+
+// call function
+toggleDarkMode()
+
+// event listenter to click checkbox
+checkbox.addEventListener('click', toggleDarkMode)
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZm1pbGxzODkiLCJhIjoiY2t3eTM4bmkwMGFvdDMxb2F1ZDhsaGswYiJ9.rbSTg0blKEIsiji9lwSKIw';
@@ -11,8 +26,15 @@ setupMap = (center) => {
         zoom: 15
       });
 
+
     const nav = new mapboxgl.NavigationControl()
         map.addControl(nav)
+
+        // toggled dark mode on map
+        checkbox.addEventListener('click', function(){
+              // if input is checked then set dark mode to map if not set light mode
+            checkbox.checked ? map.setStyle('mapbox://styles/mapbox/dark-v10') : map.setStyle('mapbox://styles/mapbox/light-v10')
+        })
 }
 
 
