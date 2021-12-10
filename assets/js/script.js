97ea97803a8362d5d3c0cf4cdf60413d8f0f058e
Tanner Brieger <tanner29@gmail.com>
2021-12-09 20:02:13 -0600

    added loop to return latlongs from each brewery in searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 
 var cityform = document.getElementById('city-form');
-var cityformInput = document.querySelector('#submit-btn')
+var cityformInput = document.querySelector('#current-location')
 
 
 
-var formSubmitHandler = function (event) {
+var formSubmit = function (event) {
     event.preventDefault();
 
     var cityname = cityformInput.value.trim();
@@ -20,8 +20,25 @@ var formSubmitHandler = function (event) {
     
 }
 
+var getCity = function(city) {
+    var requestUrl = 'https://api.openbrewerydb.org/breweries?by_city=' + city;
 
-var getbylocaltion = function (latitude,longitude) {
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data, city);
+            for (i = 0; i < data.length; i++) {
+                var longlat = ([data[i].longitude, data[i].latitude]);
+                console.log(longlat);
+                
+
+            }
+        })
+        
+}
+/*var getbylocaltion = function (latitude,longitude) {
     var requestUrl = 'https://api.openbrewerydb.org/breweries?by_dist=' + latitude + longitude;
 
     fetch(requestUrl)
@@ -32,7 +49,7 @@ var getbylocaltion = function (latitude,longitude) {
             console.log(data, latitude, longitude);
         })
     
-}
+}*/
 
 
 
@@ -43,16 +60,5 @@ var getbylocaltion = function (latitude,longitude) {
 
 
 
-var getCity = function(city) {
-    var requestUrl = 'https://api.openbrewerydb.org/breweries?by_city=' + city;
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data, city);
-        })
-    
-}
-
+cityform.addEventListener('submit', formSubmit);
