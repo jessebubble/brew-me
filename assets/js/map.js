d25aa1a12449da6618fee05dbe8cd5bceb14063c
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2021-12-10 11:56:06 -0600

    local storage setup for toggle selected

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,18 +1,59 @@
 // selectors
-var checkbox = document.querySelector('#toggle')
+var checkbox =document.querySelector('#toggle')
 var html = document.querySelector('html')
+var check = document.getElementById("check")
 
 // toggle between light and dark mode for the html page function
-var toggleDarkMode = function(){
+var toggleBetween = function(){
     // if input is checked then set dark mode if not set light mode
   checkbox.checked ? html.setAttribute('class', 'dark'): html.removeAttribute('class','dark')
 }
 
 // call function
-toggleDarkMode()
+toggleBetween()
 
 // event listenter to click checkbox
-checkbox.addEventListener('click', toggleDarkMode)
+checkbox.addEventListener('click', toggleBetween)
+
+
+// START LOCAL STORAGE
+if (localStorage.getItem('darkMode')===null) {
+    localStorage.setItem('darkMode', "false");
+}
+
+checkStatus ()
+
+    function checkStatus(){
+        if(localStorage.getItem("darkMode")==="true") {
+        //the checkbox is checked (if you load the page by default it isn’t)
+        toggle.checked = true;
+        document.getElementsByTagName("p")[0].style.color="#FFF";
+        document.body.style.backgroundColor = "#181A1B"; 
+
+        }else{
+        toggle.checked = false;
+        document.getElementsByTagName("p")[0].style.color="black";
+        document.body.style.backgroundColor = "#FFF";
+
+        }
+    }
+    // This function gets called every time the checkbox is clicked
+    function changeStatus(){   
+        //if darkMode was active and this function is called it means the user now wants light                                         
+        if (localStorage.getItem('darkMode')==="true"){    
+            //set to false, to indicate we are in light mode             
+            localStorage.setItem('darkMode', "false");   
+            document.getElementsByTagName("p")[0].style.color="black";
+            document.body.style.backgroundColor = "#FFF";
+
+        } else{
+            localStorage.setItem('darkMode', "true");                   //same code but adapted for dark theme
+            document.getElementsByTagName("p")[0].style.color="#FFF"; 
+            document.body.style.backgroundColor = "#181A1B"; 
+ 
+          }
+    }
+// END LOCAL STORAGE
 
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZm1pbGxzODkiLCJhIjoiY2t3eTM4bmkwMGFvdDMxb2F1ZDhsaGswYiJ9.rbSTg0blKEIsiji9lwSKIw';
