2b9e74faf058ba930c36d6dac188373908562411
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-12-13 10:48:22 -0600

    updated code with comments and added modal alerts

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,6 +3,12 @@ var checkbox = document.querySelector("#toggle");
 var html = document.querySelector("html");
 var cityform = document.getElementById("city-form");
 var cityformInput = document.querySelector("#current-location");
+var blueAlert = document.querySelector("#blueAlert");
+var closeBlueBtn = document.querySelector(".close-blue-btn");
+var successAlert = document.querySelector("#successAlert");
+var closeSuccessBtn = document.querySelector(".close-success-btn");
+var warningAlert = document.querySelector("#warningAlert");
+var closeWarningBtn = document.querySelector(".close-warning-btn");
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZm1pbGxzODkiLCJhIjoiY2t3eTM4bmkwMGFvdDMxb2F1ZDhsaGswYiJ9.rbSTg0blKEIsiji9lwSKIw";
@@ -15,7 +21,8 @@ var formSubmit = function (event) {
 
   if (cityname) {
     getCity(cityname);
-    save(cityname)
+    save(cityname);
+    displayAlert(cityname);
     cityformInput.value = "";
   } else {
     console.log("ENTER VALID CITY");
@@ -37,7 +44,6 @@ var getCity = function (city) {
     });
 };
 
-
 // function for map
 var setupMap = (data, city) => {
   const map = new mapboxgl.Map({
@@ -50,13 +56,6 @@ var setupMap = (data, city) => {
   const nav = new mapboxgl.NavigationControl();
   map.addControl(nav);
 
-  // toggled dark mode on map
-  checkbox.addEventListener("click", function () {
-    // if input is checked then set dark mode to map if not set light mode
-    checkbox.checked
-      ? map.setStyle("mapbox://styles/mapbox/dark-v10")
-      : map.setStyle("mapbox://styles/mapbox/light-v10");
-  });
   // iterating through data long/lat to display markers
   for (var i = 0; i < data.length; i++) {
     var lng = data[i].longitude;
@@ -66,12 +65,11 @@ var setupMap = (data, city) => {
     ll.toArray();
     console.log(ll);
 
-
     // Create a new marker.
     const marker = new mapboxgl.Marker()
       .setLngLat(ll)
       .setPopup(
-        new mapboxgl.Popup({ offset: 25}) // add popups
+        new mapboxgl.Popup({ offset: 25 }) // add popups
           .setHTML(
             `<h2>${data[i].name}</h2>
               <h2>Brewery Type: ${data[i].brewery_type}</h2>
@@ -89,55 +87,101 @@ var setupMap = (data, city) => {
 
 // START LOCAL STORAGE for toggle
 if (localStorage.getItem("darkMode") === null) {
+  localStorage.setItem("darkMode", "false");
+}
+
+function checkStatus() {
+  if (localStorage.getItem("darkMode") === "true") {
+    //the checkbox is checked (if you load the page by default it isn’t)
+    toggle.checked = true;
+    html.setAttribute("class", "dark");
+  } else {
+    toggle.checked = false;
+    html.removeAttribute("class", "dark");
+  }
+}
+// This function gets called every time the checkbox is clicked
+function changeStatus() {
+  //if darkMode was active and this function is called it means the user now wants light
+  if (localStorage.getItem("darkMode") === "true") {
+    //set to false, to indicate we are in light mode
     localStorage.setItem("darkMode", "false");
+    html.removeAttribute("class", "dark");
+  } else {
+    localStorage.setItem("darkMode", "true"); //same code but adapted for dark theme
+    html.setAttribute("class", "dark");
   }
-  
-  function checkStatus() {
-    if (localStorage.getItem("darkMode") === "true") {
-      //the checkbox is checked (if you load the page by default it isn’t)
-      toggle.checked = true;
-      html.setAttribute("class", "dark");
-    } else {
-      toggle.checked = false;
-      html.removeAttribute("class", "dark");
-    }
-  }
-  // This function gets called every time the checkbox is clicked
-  function changeStatus() {
-    //if darkMode was active and this function is called it means the user now wants light
-    if (localStorage.getItem("darkMode") === "true") {
-      //set to false, to indicate we are in light mode
-      localStorage.setItem("darkMode", "false");
-      html.removeAttribute("class", "dark");
-    } else {
-      localStorage.setItem("darkMode", "true"); //same code but adapted for dark theme
-      html.setAttribute("class", "dark");
-    }
-  }
-  // END LOCAL STORAGE for toggle
+}
+// END LOCAL STORAGE for toggle
 
 // local storage for map start
-var save = function(city){
-    var new_city = city
+var save = function (city) {
+  var new_city = city;
 
+  if (localStorage.getItem("city") === null) {
+    localStorage.setItem("city", "false");
+  }
 
-    if (localStorage.getItem("city") === null) {
-        localStorage.setItem("city", "false");
-      }
-      
-    localStorage.setItem('city',new_city)
-}
+  localStorage.setItem("city", new_city);
+};
 
-var view = function(){
-    if(localStorage.getItem("city") !== "false"){
-        var savedCity = localStorage.getItem("city");
+var view = function () {
+  var blueMsg = document.querySelector(".msgBlue");
+  if (localStorage.getItem("city") !== "false") {
+    var savedCity = localStorage.getItem("city");
 
-        getCity(savedCity)
-    }
-}
+    getCity(savedCity);
+  }
+  // alert user on instructions
+  if (localStorage.getItem("city") === null) {
+    blueAlert.classList.add("show");
+    blueAlert.classList.remove("hide");
+    blueMsg.innerHTML = `Please enter your city to see if there are any breweries in your area. `;
+    // set time out function for alert
+    setTimeout(function () {
+      blueAlert.classList.add("hide");
+      blueAlert.classList.remove("show");
+    }, 5000);
+  }
+};
 
 // local storage for map ends
 
+// Display Modal Alerts Start
+
+// function to display modal alerts
+var displayAlert = function (city) {
+  var msgSuccess = document.querySelector(".msgSuccess");
+  var msgWarning = document.querySelector(".msgWarning");
+
+  if (city) {
+    successAlert.classList.remove("hide");
+    successAlert.classList.add("show");
+    msgSuccess.innerHTML = `Success! We found some breweries in ${city}.`;
+  } else {
+    warningAlert.classList.remove("hide");
+    warningAlert.classList.add("show");
+    msgWarning.innerHTML = `Sorry, there are no breweries in your area.`;
+  }
+};
+
+// functions to delete modal alerts
+var removeBlueAlert = function () {
+  blueAlert.classList.remove("show");
+  blueAlert.classList.add("hide");
+};
+
+var removeSuccessAlert = function () {
+  successAlert.classList.remove("show");
+  successAlert.classList.add("hide");
+};
+
+var removeWarningAlert = function () {
+  warningAlert.classList.remove("show");
+  warningAlert.classList.add("hide");
+};
+
+// Display Modal Alerts End
 
 // ** need to debug **
 // if location is allowed store accurate current location
@@ -167,4 +211,9 @@ navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
 // call functions
 checkStatus();
 view();
+
+// Event Listeners
+closeBlueBtn.addEventListener("click", removeBlueAlert);
 cityform.addEventListener("submit", formSubmit);
+closeSuccessBtn.addEventListener("click", removeSuccessAlert);
+closeWarningBtn.addEventListener("click", removeWarningAlert);
